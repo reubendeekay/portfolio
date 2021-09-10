fe1418d9b922ac33e515e04ea8c2ccfd8b5874aa
reubendeekay <mrreubenyt@gmail.com>
2021-09-11 00:41:35 +0300

    Complete UI Design

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/constants.dart';
+import 'package:portfolio/sections/mainSection.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reuben',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
+        fontFamily: "Montserrat",
+        highlightColor: kPrimaryColor,
+      ),
+      home: MainPage(),
+    );
+  }
+}
---
+++ lib/sections/navBar/navBarLogo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/constants.dart';
+
+class NavBarLogo extends StatelessWidget {
+  final double height;
+  NavBarLogo({this.height});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+          MediaQuery.of(context).size.width < 1100 ? 0.0 : 20.0, 20.0, 0, 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "R",
+            style: GoogleFonts.poppins(
+                fontSize: 60,
+                color: kPrimaryColor,
+                fontWeight: FontWeight.w900),
+          ),
+          Text(
+            ".",
+            style: GoogleFonts.notoSans(
+                fontSize: 60, color: Colors.white, fontWeight: FontWeight.w900),
+          ),
+        ],
+      ),
+    );
+  }
+}
